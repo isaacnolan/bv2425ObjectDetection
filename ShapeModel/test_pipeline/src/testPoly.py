52a417512cb5c40327bd3b5b78d100b6ae73758e
Sean Broderick <133047023+seanbrod@users.noreply.github.com>
2024-10-23 19:46:27 -0400

    Update testPoly.py

--- ShapeModel/test_pipeline/src/testPoly.py
+++ ShapeModel/test_pipeline/src/testPoly.py
@@ -1,24 +1,27 @@
-# Adarsha's script for testing several models at once
 import cv2
+import sys
 import time
 import psutil
 from ultralytics import YOLO
+import os
 
+#FUNCTIONS 
 # Validate the model
-def validation(model):
-    metrics = model.val(data = "",
-                        imgsz = 640,
-                        batch = 16,
-                        save_hybrid = True,
-                        conf = .01,
-                        device = "cpu",
-                        plots = True,
-                        save_json=True)  # no arguments needed, dataset and settings remembered
+def validation(model, dataset, configuration):
+    metrics = model.val(data = dataset,
+                        imgsz = configuration["imgsz"],
+                        batch = configuration["batch"],
+                        save_hybrid = configuration["save_hybrid"],
+                        conf = configuration["conf"],
+                        device = configuration["device"],
+                        plots = configuration["plots"],
+                        save_json = configuration["save_json"]) 
     metrics.box.map  # map50-95
     metrics.box.map50  # map50
     metrics.box.map75  # map75
     metrics.box.maps  # a list contains map50-95 of each category
-    
+
+#process fram for performance
 def process_frame(model, frame, results_memory, results_time, model_name):
         initial_memory = get_ram_usage()
         start_time = time.perf_counter()
@@ -37,20 +40,49 @@ def get_ram_usage():
     memory_info = process.memory_info()
     return memory_info.rss / (1024 * 1024)  # Convert to MB
 
+#process video
+def process_video(video_path, model, results_memory, results_time, model_name):
+    video = cv2.VideoCapture(video_path)
+    if not video.isOpened():
+        print("Error opening video file")
+
+    # Loop through the video frames
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        process_frame(model, frame, results_memory, results_time, model_name)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture object and close any open windows
+    video.release()
+    cv2.destroyAllWindows()
+
+#process images
+def process_image(images_path, model, results_memory, results_time, model_name):
+    directory = images_path
+
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            frame = cv2.imread(f)
+            if frame is None:
+                print("Error: Could not load image.")
+                return
+            else:
+                process_frame(model, frame, results_memory, results_time, model_name)
+    
+    
+#______________________________________________________________________________________________________
 
 def main():
     # Initialize models
-    models_directory = 'C:\\Users\\adars\\repos\\bv2425ObjectDetection\\ShapeModel\\testing\\misc\\'
+    models_directory = 'ShapeModel/test_pipeline/models/'
     modelNames = ['yolo11n', 'yolo11s', 'yolo11m', 'yolo11l', 'yolo11x']
-
-    # Load the image from file
-    image_path = 'C:\\Users\\adars\\repos\\bv2425ObjectDetection\\ShapeModel\\testing\\data\\basketball_court.jpg'
-
-    frame = cv2.imread(image_path)
-
-    if frame is None:
-        print("Error: Could not load image.")
-        return
 
     results_memory = {
          "yolo11n": [],
@@ -68,20 +100,59 @@ def main():
          "yolo11x": []
     }
 
+    val_configuration = {
+        "imgz": 640,
+        "batch": 16,
+        "save_hybrid": True,
+        "conf": .5,
+        "device": "cpu",
+        "plots": True,
+        "save_json": True
+    }
+
+    dataset_path = "ShapeModel/test_pipeline/data"
+    dataset_yaml = dataset_path + "/hi.yaml"
+
+    #User Inputs: 
+    #ask if they want default configuration or not for validation?
+    config = input("Do you want default configuration? (Y/n): \n")
+    if config == 'N'or config == 'n':
+        for key, value in val_configuration.items():
+            val_configuration[key] = input('{}: '.format(key))
+    else:
+        print("Default Configuration for Validation\n")
+
+    #ask if they testing on images or video?
+    data_type = input("Video or Images? (V/I): \n")
+
+    if data_type == 'V' or data_type == 'v':
+        print("Video Data Selected/n")
+    elif data_type == 'I' or data_type == 'i':
+        print("Image Data Selected/n")
+    else:
+        print("Invalid Data Type/n")
+        sys.exit(1)
 
     # Loop through each model
     for model_name in modelNames:
         print(f"Testing model: {model_name}")
         model_path = models_directory + model_name
-
         # Load model
         model = YOLO(model_path)
-
-        # TODO: video loop
-
-        process_frame(model, frame, results_memory, results_time, model_name) 
+        
+        
+        #video and image processing for each model
+        if data_type == 'V' or data_type == 'v':
+            video_path = dataset_path
+            process_video(video_path, model, results_memory, results_time, model_name)
+        else:
+            process_image(dataset_path, model, results_memory, results_time, model_name)
+            #run accuracy check 
+            validation(model, dataset_yaml, val_configuration)
+ 
        
 
+    #TODO: display avgs and all data cleanly
     #Note: display average instead of individual
     # Print out the time data
     for result in      results_time:
