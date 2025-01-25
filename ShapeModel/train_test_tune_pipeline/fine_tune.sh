11584fece7808a5ae60838fab3b8f63a144e4a30
EashanVytla <Eashan.Vytla@gmail.com>
2025-01-25 16:16:43 -0500

    Adding the bash script

---
+++ ShapeModel/train_test_tune_pipeline/fine_tune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BATCH --job-name=ShapeColorModelTraining
+#SBATCH --nodes=1 --ntasks-per-node=28 --gpus-per-node=1 --gpu_cmode=shared
+#SBATCH --time=8:00:00
+#SBATCH --account=PAS2152
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load miniconda3/23.3.1-py310
+
+module list
+
+source activate yolo_test
+
+echo "Python executable: $(which python)"
+
+python3 fine_tune.py
